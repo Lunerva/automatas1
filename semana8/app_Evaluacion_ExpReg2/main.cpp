608c105b0b0be51e444a6afcf5646981310e7336
Luis Barranco <135748715+Lunerva@users.noreply.github.com>
2024-04-14 21:52:29 -0600

    fintareasemana8

--- semana8/app_Evaluacion_ExpReg2/main.cpp
+++ semana8/app_Evaluacion_ExpReg2/main.cpp
@@ -5,9 +5,9 @@
 using namespace std;
 
 int main(int argc, char *argv[]){
+    /*
     QString cadena = "1010";
     QRegularExpression patron("^[0-1]{1,8}$");
-
     QRegularExpressionMatch match = patron.match(cadena);
 
     if(match.hasMatch()){
@@ -15,12 +15,56 @@ int main(int argc, char *argv[]){
     }else{
         cout<<"numero binario invalido"<<endl;
     }
+    */
 
+    cout<<"\n21130876 Luis Barranco\n";
 
+    QString cad_opc[] = {
+            "jose@correo.es",
+            "john.pherez@gmail.co.uk",
+            "jose.jose@outlook.com",
+            "josefan@mx",
+            "joss@localhost"
+        };
+
+    for (int i = 0; i < sizeof(cad_opc) / sizeof(cad_opc[0]); ++i) {
+        cout<<"\ncadena de entrada: "<<cad_opc[i].toStdString()<<endl;
+
+        QString qstr_patron_opc1 = "^[\\w-]+(\\.?[\\w-]+)@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)(\\.[A-Za-z]{2,})?$";
+        QRegularExpression patron_opc1(qstr_patron_opc1);
+        cout<<"\nOPCION 1\npatron 1: "<<qstr_patron_opc1.toStdString()<<endl;
+        QRegularExpressionMatch match_opc1 = patron_opc1.match(cad_opc[i]);
+
+        if(match_opc1.hasMatch()){
+            cout<<"correo valido "<<i+1<<endl;
+        }else{
+            cout<<"correo invalido "<<i+1<<endl;
+        }
+
+        QString qstr_patron_opc2 = "^[\\w-]+(\\.[\\w-_]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+        QRegularExpression patron_opc2(qstr_patron_opc2);
+        cout<<"\nOPCION 2\npatron 2: "<<qstr_patron_opc2.toStdString()<<endl;
+        QRegularExpressionMatch match_opc2 = patron_opc2.match(cad_opc[i]);
+        if (match_opc2.hasMatch()) {
+            cout<<"correo valido "<<i+1<<endl;
+        }else{
+            cout<<"correo invalido "<<i+1<<endl;
+        }
+
+        QString qstr_patron_opc3 = "[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}";
+        QRegularExpression patron_opc3(qstr_patron_opc3);
+        cout << "\nOPCION 3\npatron 3: " << qstr_patron_opc3.toStdString() << endl;
+        QRegularExpressionMatch match_opc3 = patron_opc3.match(cad_opc[i]);
+        if(match_opc3.hasMatch()){
+            cout<<"correo valido "<<i+1<<endl;
+        }else{
+            cout<<"correo invalido "<<i+1<<endl;
+        }
+    }
 
     return 0;
-}
 
+}
 
 
 
