a5178d26b0397072c2c9b4e8873581eb71d44d74
luis <nezzlu87@gmail.com>
2024-04-28 20:04:28 -0600

    fin:semana9

--- semana9/app_validarParString/main.cpp
+++ semana9/app_validarParString/main.cpp
@@ -4,53 +4,121 @@
 
 using namespace std;
 
+
+// MI SOLUCION
+bool analizarCadPar(string valor){
+    bool val = false;
+    for(int i=0;i<valor.size();i++){
+        switch(valor.at(i)){
+            case '0':
+            case '2':
+            case '4':
+            case '6':
+            case '8':
+                //cout << "Valido par" << endl;
+                val = true;
+                break;
+            default:
+                //cout << "No valido par" << endl;
+                val= false;
+                break;
+        }
+    }
+    return val;
+}
+
+// SOLUCION 2 USANDO AUTOMATA
+
+bool validarPar(string cadena){
+    const char par[6] = "02468";
+//    const char non[6] ="13579";
+    // Definición de variables
+    char carEntrada; // Carácter de entrada actual
+    int edoActual = 0; // Estado actual del autómata
+    int i = 0; // Índice para recorrer la cadena
+    bool band = false; // Bandera para indicar si se encontró un número par
+
+    for(i =0;i<cadena.length();i++){
+        switch(edoActual){
+        case 0:
+            carEntrada=cadena.at(i);
+            if(strchr(par, carEntrada)){
+                edoActual=1;
+                band=true;
+            }
+            break;
+        case 1:
+            carEntrada=cadena.at(i);
+            if(strchr(par, carEntrada)){
+                band=true;
+            }else{
+                edoActual=0;
+                band=false;
+            }
+            break;
+        }
+    }
+
+    return band;
+}
+
+
+// SOLUCION EN CLASE
 const char par[6] = "02468";
 const char non[6] ="13579";
 
-bool analizarNumero(char *cadena){
-    char carEntrada;
-    int edoActual =0;
-    int i=0;
-    bool band = false;
-    while(i<strlen(cadena)){
-        switch(edoActual){
-            case 0:{
+bool analizarNumero(char *cadena) {
+    // Definición de variables
+    char carEntrada; // Carácter de entrada actual
+    int edoActual = 0; // Estado actual del autómata
+    int i = 0; // Índice para recorrer la cadena
+    bool band = false; // Bandera para indicar si se encontró un número par
+
+    // Ciclo while para recorrer la cadena
+    while (i < strlen(cadena)) {
+        // Switch para manejar los diferentes estados del autómata
+        switch (edoActual) {
+            case 0: {
+                // Estado 0: Inicial, se determina si el carácter es par o impar
                 carEntrada = cadena[i++];
-                if(strchr(par,carEntrada)){
-                    edoActual=1;
-                }else if(strchr(non,carEntrada)){
-                    edoActual=2;}
-            }
-            break;
-            case 1:{
-                carEntrada=cadena[i++];
-                if(strchr(par,carEntrada)){
-                    edoActual=1;
-                }else if(strchr(non,carEntrada)){
-                    edoActual=2;
-                }else{
-                    edoActual=3;
-                    //reprensenta el asterisco o el retroceso
+                if (strchr(par, carEntrada)) {
+                    edoActual = 1; // Si es par, pasa al estado 1
+                } else if (strchr(non, carEntrada)) {
+                    edoActual = 2; // Si es impar, pasa al estado 2
                 }
             }
             break;
-            case 2:{
-                carEntrada=cadena[i++];
-                if(strchr(par,carEntrada)){
-                    edoActual=1;
-                }else if(strchr(non,carEntrada)){
-                    edoActual=2;
+            case 1: {
+                // Estado 1: Carácter anterior era par, se verifica el siguiente carácter
+                carEntrada = cadena[i++];
+                if (strchr(par, carEntrada)) {
+                    edoActual = 1; // Si es par, se mantiene en el estado 1
+                } else if (strchr(non, carEntrada)) {
+                    edoActual = 2; // Si es impar, pasa al estado 2
+                } else {
+                    edoActual = 3; // Si es otro carácter, pasa al estado 3
                 }
             }
             break;
-            case 3:
-            {
-                cout<<"numero par"<<endl;
-                band=true;
-            }break;
+            case 2: {
+                // Estado 2: Carácter anterior era impar, se verifica el siguiente carácter
+                carEntrada = cadena[i++];
+                if (strchr(par, carEntrada)) {
+                    edoActual = 1; // Si es par, pasa al estado 1
+                } else if (strchr(non, carEntrada)) {
+                    edoActual = 2; // Si es impar, se mantiene en el estado 2
+                }
+            }
+            break;
+            case 3: {
+                // Estado 3: Se ha encontrado un número par
+                cout << "Número par" << endl;
+                band = true; // Se marca la bandera como verdadera
+            }
+            break;
         }
     }
-    return band;
+    return band; // Devuelve la bandera que indica si se encontró un número par
 }
 
 
@@ -58,23 +126,35 @@ int main()
 {
 
     //IMPLEMENTAR LA APLICACION PARA EL DOMING 28 DE ABRIL 2024
+    //USANDO LA PROPUESTA EN CLASE
 
 
-
-    string cad="";
+///////////PROPUESTA 1
+    char *cad = new char[10];
+    cout<<"App para determinar si la cadena de caracteres es par o impar, "
+          "usando un automata finito determinista.\nIngrese el valor: "<<endl;
     cin>>cad;
+    cout<<"el valor: "<<cad
+       <<"\nSi es 0 es impar, si es 1 es par: "<<analizarNumero(cad)<<endl;
+
+///////////PROPUESTA 2
+    string valor;
+    cout<<"\n\n\nApp para determinar si la cadena de caracteres es par o impar, "
+          "\nIngrese el valor: "<<endl;
+    cin>>valor;
+    cout<<"el valor: "<<valor
+       <<"\nSi es 0 es impar, si es 1 es par: "<<analizarCadPar(valor)<<endl;
+
+///////////PROPUESTA 3
+
+    cout<<"\n\n\nApp para determinar si la cadena de caracteres es par o impar, "
+              "usando un automata finito determinista.\nIngrese el valor: "<<endl;
+    //cin>>valor;
+    cout<<"el valor: "<<valor
+           <<"\nSi es 0 es impar, si es 1 es par: "<<validarPar(valor)<<endl;
 
 
-    for(int i=0;i<cad.length();i++){
-        switch(cad.at(i)){
-            case ( "0" | "2" | "4" | "6" | "8" ):
-            cout<<"valido par"<<endl;
-            break;
-            deafult:
-            cout<<"no valido no numero"<<endl;
-            break;
-        }
-    }
+
 
     return 0;
 }
